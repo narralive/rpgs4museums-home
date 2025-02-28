7733b70f8ddb77961b12298a7e6fb70dedeb4fa4
LoriKougiou <44337997+lorikou@users.noreply.github.com>
2025-02-28 19:35:29 +0200

    Added About and Publications pages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2406094322"
+    serviceWorkerVersion: "2717662816"
   }
 });
